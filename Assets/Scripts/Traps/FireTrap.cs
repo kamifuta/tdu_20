fd35806a902ee7d955f19f69c735069b5cef7ed0
kamifuta <kamifuta010@outlook.jp>
2021-03-05 23:54:35 +0900

    もろもろの追加

---
+++ Assets/Scripts/Traps/FireTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrap : MonoBehaviour
+{
+    private GameObject Fire_EF_Obj;
+    private ParticleSystem Fire_EF;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fire_EF_Obj= Resources.Load<GameObject>("Fire");
+
+        Fire_EF = Instantiate(Fire_EF_Obj, transform.position, Quaternion.identity).GetComponent<ParticleSystem>();
+        Fire_EF.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
