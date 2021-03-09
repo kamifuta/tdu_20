7724e08e64fa48ee7e6ea25c2079feac0a9d5ac1
suraimu <s.r.131117@outlook.jp>
2021-03-09 23:24:21 +0900

    IceTrap
    氷のトラップ(途中)

---
+++ Assets/Scripts/Traps/IceTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IceTrap : Trap
+{
+    public Image IceImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnAwake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void OnAwake()
+    {
+        Instantiate(IceImage, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+
+
+    }
+}
