3cfcd8a0916a1beed6d111d341ec0cfb6eed9c48
suraimu <s.r.131117@outlook.jp>
2021-04-13 23:34:27 +0900

    泡（途中）

--- Assets/Scripts/Traps/BubbleTrap.cs
+++ Assets/Scripts/Traps/BubbleTrap.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class BubbleTrap : Trap
 {
-    public Image BubbleImage;
+    public GameObject canvas;
+    public GameObject BubbleT;
     float BubbleHp = 0;
     // Start is called before the first frame update
     void Start()
     {
         BubbleHp = Random.Range(6, 8);
+        OnAwake();
     }
 
     // Update is called once per frame
@@ -22,7 +24,9 @@ public class BubbleTrap : Trap
     {
         for (int i = 0; i < BubbleHp; i++)
         {
-            Instantiate(BubbleImage);
+            GameObject prefab = (GameObject)Instantiate(BubbleT, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            prefab.transform.SetParent(canvas.transform, false);
+
         }
 
         
