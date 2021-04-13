c3aa929ee646be0c5fd249b13db19e561ba01f1b
Kazuki Tanida <tanida20rd119@gmail.com>
2021-04-13 23:29:42 +0900

    移動トラップのスクリプト追加

---
+++ Assets/Scripts/Traps/DownMoveTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownMoveTrap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(0.0f, 0.0f, Random.Range(-10.0f, -20.0f));
+        rb.AddForce(force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Traps/LeftMoveTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMoveTrap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(Random.Range(-10.0f, -20.0f), 0.0f, 0.0f);
+        rb.AddForce(force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
