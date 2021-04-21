e7c194cba06eb3116171c2a0fd494fa9f4b7a9d6
Kazuki Tanida <kazu1323.hjn@icloud.com>
2021-04-21 22:09:24 +0900

    アニメーション完成や！

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,12 +6,14 @@ public class PlayerController : MonoBehaviour
     private PlayerInput input;
     private PlayerAction playerAction;
     private Rigidbody rb;
+    private Animator _animator;
 
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
         playerAction = GetComponent<PlayerAction>();
         rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
 
@@ -20,13 +22,25 @@ public class PlayerController : MonoBehaviour
     {
         if (input.moveVec != Vector3.zero)
         {
+            
             rb.velocity = input.moveVec.normalized;
             if (input.PushedDash)
             {
                 rb.velocity = input.moveVec.normalized * 2;
+                _animator.SetBool("is_running",true);
+                _animator.SetBool("is_walking", false);
+            }
+            else
+            {
+                _animator.SetBool("is_walking", true);
+                _animator.SetBool("is_running", false);
             }
         }
-
+        else
+        {
+            _animator.SetBool("is_walking", false);
+            _animator.SetBool("is_running", false);
+        }
         if (input.moveVec.x != 0 || input.moveVec.z != 0)
         {
             transform.LookAt(transform.position + input.moveVec);
