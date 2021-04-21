5445b97a5a15709b419fb1427fe1deb85d2d189c
suraimu <s.r.131117@outlook.jp>
2021-04-21 13:19:56 +0900

    調整

--- Assets/Scripts/Traps/BubbleTrap.cs
+++ Assets/Scripts/Traps/BubbleTrap.cs
@@ -7,24 +7,29 @@ public class BubbleTrap : Trap
 {
     public GameObject canvas;
     public GameObject BubbleT;
+    private Sprite BubbleS;
     float BubbleHp = 0;
     // Start is called before the first frame update
     void Start()
     {
         BubbleHp = Random.Range(6, 8);
-        OnAwake();
+       // OnAwake();
+        BubbleS = BubbleT.GetComponent<SpriteRenderer>().sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            OnAwake();
+        }
     }
     protected override void OnAwake()
     {
         for (int i = 0; i < BubbleHp; i++)
         {
-            GameObject prefab = (GameObject)Instantiate(BubbleT, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            GameObject prefab = (GameObject)Instantiate(BubbleT, new Vector3(0.0f, 0.0f,Random.Range(-8,10)), Quaternion.identity);
             prefab.transform.SetParent(canvas.transform, false);
 
         }
