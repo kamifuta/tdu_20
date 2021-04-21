7228c77f37ec90a55ea36387b54198a7838e428f
Ryosuke Ishida <gakusyan14@ezweb.ne.jp>
2021-04-22 00:30:59 +0900

    帽子の色変更

---
+++ Assets/Scripts/Matching/ColorChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField] private Slider redSlider = null;
+    [SerializeField] private Slider greenSlider = null;
+    [SerializeField] private Slider blueSlider = null;
+
+    [SerializeField] private GameObject hat = null;
+    Material hatmat;
+
+    Color hatcolor;
+
+    void Awake()
+    {
+        hatmat = hat.GetComponent<Renderer>().material;
+        Observable.CombineLatest(redSlider.OnValueChangedAsObservable(), greenSlider.OnValueChangedAsObservable(), blueSlider.OnValueChangedAsObservable())
+            .Select(colorList => hatcolor =  new Color(colorList[0], colorList[1], colorList[2])).Subscribe(color => hatmat.color = color);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(hatmat);
+    }
+}
